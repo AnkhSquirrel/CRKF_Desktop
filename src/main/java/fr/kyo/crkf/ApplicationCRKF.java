d60d86945e91f741d740b84bacc3a3d2293fd14e
OrelienThomas <orelien592@hotmail.fr>
2022-09-02 08:42:51 +0200

    Link "detail instrument" + show instrument info on it got to "detail instrument" page  when you selected one instrument in "list instrument" thi page show all the instrument info

--- src/main/java/fr/kyo/crkf/ApplicationCRKF.java
+++ src/main/java/fr/kyo/crkf/ApplicationCRKF.java
@@ -1,12 +1,16 @@
 package fr.kyo.crkf;
 
 import fr.kyo.crkf.Controller.AccueilController;
+import fr.kyo.crkf.Controller.DetailInstrumentController;
+import fr.kyo.crkf.Controller.InstrumentController;
 import fr.kyo.crkf.Controller.NavbarController;
+import fr.kyo.crkf.Entity.Instrument;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.MenuBar;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
@@ -80,6 +84,8 @@ public class ApplicationCRKF extends javafx.application.Application {
             FXMLLoader fxmlLoaderListeInstrument = new FXMLLoader();
             fxmlLoaderListeInstrument.setLocation(ApplicationCRKF.class.getResource("liste_instrument.fxml"));
             GridPane listeInstrument = fxmlLoaderListeInstrument.load();
+            InstrumentController instrumentController = fxmlLoaderListeInstrument.getController();
+            instrumentController.setApplicationCRKF(this);
 
             mainWindow.setCenter(listeInstrument);
         } catch (IOException e) {
@@ -93,6 +99,20 @@ public class ApplicationCRKF extends javafx.application.Application {
         alert.setHeaderText("CRKF est un outil de gestion pour conservatoire");
         alert.setContentText("K.Y.O");
         alert.showAndWait();
+    }
+    public void openDetailInstrument(Instrument instrument){
+        try {
+            FXMLLoader fxmlLoaderListeInstrument = new FXMLLoader();
+            fxmlLoaderListeInstrument.setLocation(ApplicationCRKF.class.getResource("detail_instrument.fxml"));
+            AnchorPane detailInstrument = fxmlLoaderListeInstrument.load();
+            DetailInstrumentController detailInstrumentController = fxmlLoaderListeInstrument.getController();
+            detailInstrumentController.setApplicationCRKF(this);
+            detailInstrumentController.setInstrument(instrument);
+
+            mainWindow.setCenter(detailInstrument);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
