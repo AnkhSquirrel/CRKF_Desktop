847d328aa83a5188483a6601a59434881697fb6c
Yoann-Lgd <82641397+Yoann-Lgd@users.noreply.github.com>
2022-09-07 15:25:19 +0200

    Method addEcole (to do)

--- src/main/java/fr/kyo/crkf/controller/CreateEcoleModalController.java
+++ src/main/java/fr/kyo/crkf/controller/CreateEcoleModalController.java
@@ -1,7 +1,12 @@
 package fr.kyo.crkf.controller;
 
+import fr.kyo.crkf.Entity.Adresse;
+import fr.kyo.crkf.Entity.Departement;
+import fr.kyo.crkf.Entity.Ecole;
+import fr.kyo.crkf.Entity.Ville;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -10,10 +15,24 @@ public class CreateEcoleModalController {
     @FXML
     private Button ajouter;
     @FXML
-    private TextField nom;
+    private TextField nomEcole;
+    @FXML
+    private TextField libeleAdresse;
+    @FXML
+    private ComboBox<Ville> villeComboBox;
+    @FXML
+    private ComboBox<Departement> departementComboBox;
+    @FXML
+    private ComboBox<Departement> numDepartement;
 
 
-
+    @FXML
+    private void addEcole(){
+        Ecole ecole = new Ecole(0, nomEcole.getText(),
+                new Adresse(0,libeleAdresse.getText(),
+                new Ville(0, villeComboBox.getSelectionModel().getSelectedItem().toString(),0F, 0F,
+                new Departement(0, numDepartement.getSelectionModel().getSelectedItem().toString() , departementComboBox.getSelectionModel().getSelectedItem().toString()))));
+    }
     public void setModal(Stage modal) {
         this.modal = modal;
     }
