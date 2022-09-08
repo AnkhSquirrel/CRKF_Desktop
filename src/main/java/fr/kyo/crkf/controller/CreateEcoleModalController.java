c4d94ff12b3fe952356b6367aee129050d811f72
Yoann-Lgd <82641397+Yoann-Lgd@users.noreply.github.com>
2022-09-08 16:12:40 +0200

    Modification of liste_ecole.fxml, modal_ecole.fxml files Method addEcole (to do)

--- src/main/java/fr/kyo/crkf/controller/CreateEcoleModalController.java
+++ src/main/java/fr/kyo/crkf/controller/CreateEcoleModalController.java
@@ -4,11 +4,14 @@ import fr.kyo.crkf.Entity.Adresse;
 import fr.kyo.crkf.Entity.Departement;
 import fr.kyo.crkf.Entity.Ecole;
 import fr.kyo.crkf.Entity.Ville;
+import fr.kyo.crkf.Searchable.Filter;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.controlsfx.control.SearchableComboBox;
 
 public class CreateEcoleModalController {
     private Stage modal;
@@ -19,19 +22,26 @@ public class CreateEcoleModalController {
     @FXML
     private TextField libeleAdresse;
     @FXML
-    private ComboBox<Ville> villeComboBox;
+    private SearchableComboBox<Ville> ville;
     @FXML
-    private ComboBox<Departement> departementComboBox;
+    private SearchableComboBox<Departement> nomDepartement;
     @FXML
-    private ComboBox<Departement> numDepartement;
+    private SearchableComboBox<Departement> numDepartement;
+    private Filter filter;
 
 
+    @FXML
+    private void initialize(){
+        filter = new Filter();
+        nomDepartement.setItems(FXCollections.observableArrayList(filter.getDepartements()));
+        ville.setItems(FXCollections.observableArrayList(filter.getVilles()));
+    }
     @FXML
     private void addEcole(){
         Ecole ecole = new Ecole(0, nomEcole.getText(),
                 new Adresse(0,libeleAdresse.getText(),
-                new Ville(0, villeComboBox.getSelectionModel().getSelectedItem().toString(),0F, 0F,
-                new Departement(0, numDepartement.getSelectionModel().getSelectedItem().toString() , departementComboBox.getSelectionModel().getSelectedItem().toString()))));
+                new Ville(0, ville.getSelectionModel().getSelectedItem().getVille(),0F, 0F,
+                new Departement(0, numDepartement.getSelectionModel().getSelectedItem().toString() , nomDepartement.getSelectionModel().getSelectedItem().toString()))));
     }
     public void setModal(Stage modal) {
         this.modal = modal;
